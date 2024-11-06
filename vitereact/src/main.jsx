10ab65550fdeca9c871c3ee9d896b0cb3f90c2ef
Akul1306 <akul60314@gmail.com>
2024-11-06 22:23:49 +0530

    main react

--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>hiiiiii</h1>
+    </div>
+  );
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://www.google.com/",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+const anotherElement = (
+  <a href="google.com" target="_blank">
+    Visit google
+  </a>
+);
+const ReactElement = React.createElement("a", {
+  href: "google.com",
+  target: "_blank"},
+  'click me to visit google '
+);
+createRoot(document.getElementById("root")).render(ReactElement);
