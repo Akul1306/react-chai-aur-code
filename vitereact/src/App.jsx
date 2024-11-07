5e00dae1f1a2dd9c085214635374f53dc2c5327c
Akul1306 <akul60314@gmail.com>
2024-11-07 10:48:47 +0530

    basic complete

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -2,10 +2,11 @@ import Chai from "./chai";
 // import "./App.css";
 
 function App() {
+  const name = "akul";
   return (
     <>
       <Chai />
-      <h1>Hello guys</h1>
+      <h1>Hello {name}</h1>
     </>
   );
 }
--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -24,9 +24,12 @@ const anotherElement = (
     Visit google
   </a>
 );
+const anotherUser = "Akul"
 const ReactElement = React.createElement("a", {
   href: "google.com",
   target: "_blank"},
-  'click me to visit google '
+  'click me to visit google ',
+  anotherUser
 );
-createRoot(document.getElementById("root")).render(ReactElement);
+createRoot(document.getElementById("root")).render(ReactElement
+);
