6d207b0dd103a7d86d796d117cff00f9c44fd6ea
Akul1306 <akul60314@gmail.com>
2024-11-11 21:58:55 +0530

    17:34

--- aichatbot/src/App.jsx
+++ aichatbot/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [question, setQuestion] = useState("");
+
+  async function generateAnswer() {
+    console.log("loading");
+    const response = await axios({
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBmKOwDKmWNfTQ3ZC9Mt3evtY4RAYTi8Ao",
+      method: "post",
+      data: {
+        contents: [{ parts: [{ text: question }] }],
+      },
+    });
+    console.log(
+      response["data"]["candidates"][0]["content"]["parts"][0]["text"]
+    );
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>AI Chatbot</h1>
+      <textarea
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+        cols="30"
+        rows="10"
+      ></textarea>
+      <button onClick={generateAnswer}>Generate answer</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
